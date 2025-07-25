991090daf419061f25afcf2465f46f62751a0e79
Sandeep Kumar <skumar131431@gmail.com>
2025-07-25 23:19:59 +0530

    bc

--- src/utils/axiosClient.js
+++ src/utils/axiosClient.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 const axiosClient =  axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: 'https://code-ace-backend1.onrender.com',
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json'
