3bc051936237f6b5ea4fee21e7c2da074b62ab46
Sandeep Kumar <skumar131431@gmail.com>
2025-07-25 19:39:12 +0530

    send

--- src/index.js
+++ src/index.js
@@ -19,6 +19,9 @@ app.use(cors({
 app.use(express.json());
 app.use(cookieParser());
 
+app.get('/',(req,res)=>{
+    res.send("working")
+});
 app.use('/problem',problemRouter);
 app.use('/contest',contestRouter);
 
